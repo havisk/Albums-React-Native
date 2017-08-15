07d2e409aa96209ff49295722478652cc55111a2
Kool Havis <havisk@gmail.com>
2017-08-15 09:41:33 -0400

    initial, added header

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+//index.ios.js - place code in here for ios
+
+//import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+
+//create a component
+const App = () => {
+  return (
+      <Header/>
+
+  );
+};
+
+//render it to the device
+AppRegistry.registerComponent('albums', () => App);
+
+
